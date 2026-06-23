6b2eceedb6b45e9f2c778ae28111092adf84d8bc
anames2323 <137616037+anames2323@users.noreply.github.com>
2026-06-23 10:18:15 +1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+token = '8762089094:AAGvv2yrQONhEauSJgwcL4l5GrLlK9JrK9c'  # Токен бота
+NICNAME_BOT = 'anonimmassagingbot'  # username бота без @
+
+# ID ГЛАВНОГО АДМИНИСТРАТОРА (ВАШ ID)
+# Получить свой ID можно у бота @getmyid_bot
+MAIN_ADMIN_ID = 8795006636  # 👈 ЗАМЕНИТЕ НА СВОЙ ID!
